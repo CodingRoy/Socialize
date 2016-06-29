8d7d454b8ba61294caab935122707c044dbbd6c0
Roy <debessjes@hotmail.com>
2016-06-29 21:23:19 +0200

    Update index.php

--- view/index/index.php
+++ view/index/index.php
@@ -1,11 +1,11 @@
 <p class=""> Welcome to Socialize! </p>
 <p class=""> Join us here! </p>
 <form action="../user/create" method="post">
-	<p class="">Username</div>
+	<p class="">Username</p>
 	<input class="" type="text" name="username" placeholder="Username">
-	<p class="">Email Address</div>
+	<p class="">Email Address</p>
 	<input class="" type="text" name="email" placeholder="Email Address">
-	<p class="">Create password</div>
+	<p class="">Create password</p>
 	<input class="" type="password" name="password" placeholder="Create password">
 	<input type="submit" class="" value="Register">
 </form>
