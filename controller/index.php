e2c4a25ec40e261dd9445e39d05ae128a4cc80a8
Roy <debessjes@hotmail.com>
2016-06-30 15:52:14 +0200

    Bug for not using tab?
    i don't know what happend the file was exactly like this :smile:

--- controller/index.php
+++ controller/index.php
@@ -2,8 +2,8 @@
 class Index extends Controller {
 	function __construct() {
 		parent::__construct();
-      Session::init();
-      $logged = Session::get('loggedIn');
+			Session::init();
+			$logged = Session::get('loggedIn');
 	}
 
 	function index() {
