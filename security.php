f7b59fb8557ec774c33ad9d259c41c4b15fc1dd4
Roy <debessjes@hotmail.com>
2016-07-02 15:01:15 +0200

    security.php Example
    An example for how to make the security.php file. Some more info is in
    the file

---
+++ security.php
@@ -0,0 +1,12 @@
+<?php
+define(DB_TYPE, "mysql");
+define(DB_HOST, "localhost");
+define(DB_NAME, "Databasename");
+define(DB_USER, "root");
+define(DB_PASS, "Example123");
+define(PASSWORD_HASH, 'Yourcustomsentensehere!');
+
+/*!IMPORTANT! This is an example file on how to setup your database connection, your database type can be different. Your database name, user and password should be different!
+* The PASSWORD_HASH can contain letters, digits and special characters so make it however you like, but once you start using it don't change it!
+* Users who register won't be able to login anymore if the PASSWORD_HASH changes!!
+* IT IS HIGHLY RECOMMENDED TO MOVE THIS FILE AT LEAST DIRECTORY UP!! for security reasons. */
