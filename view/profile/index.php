2cf7fc2a8aca5bb6a00076cfb8e93e05fafdea5b
Roy <debessjes@hotmail.com>
2016-07-29 23:50:15 +0200

    simple profile page more to come
    more userinfo will come later

---
+++ view/profile/index.php
@@ -0,0 +1,4 @@
+<?php foreach($this->userinfo as $key => $value):?>
+<p class="">Welcome to the profile of <?=$value['username']?></p>
+<?php endforeach;?>
+<p class="content"> Nothing to find here, yet. </p>
---
+++ view/profile/private.php
@@ -0,0 +1,4 @@
+<?php foreach($this->userinfo as $key => $value):?>
+<p class="">Welcome to your profile <?=$value['username']?></p>
+<?php endforeach;?>
+<p class="content"> Nothing to find here, yet. </p>
