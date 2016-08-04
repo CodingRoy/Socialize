57ebc8abc269f6b5e1ec489fa07dd34112c997c7
Roy <debessjes@hotmail.com>
2016-08-04 20:49:08 +0200

    Update security.php

--- security.php
+++ security.php
@@ -4,7 +4,7 @@ define("DB_HOST", "localhost");
 define("DB_NAME", "Databasename");
 define("DB_USER", "root");
 define("DB_PASS", "Example123");
-define('" PASSWORD_HASH", 'Yourcustomsentensehere!');
+define("PASSWORD_HASH", 'Yourcustomsentensehere!');
 
 /*!IMPORTANT! This is an example file on how to setup your database connection, your database type can be different. Your database name, user and password should be different!
 * The PASSWORD_HASH can contain letters, digits and special characters so make it however you like, but once you start using it don't change it!
