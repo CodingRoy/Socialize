1645521767ca4edc9eb9d4ed927ca41c8e5ed362
CodingRoy <debessjes@hotmail.com>
2016-10-04 19:31:55 +0200

    Road to Socialize 0.7.0 username validation

--- public/js/validate.js
+++ public/js/validate.js
@@ -3,10 +3,20 @@ var u = 0;
 
 function un_val() {
   var error1 = document.getElementById("error1");
+  var un = document.forms["Register"]["username"].value;
   var un_l = document.forms["Register"]["username"].value.length;
+  var format = /^[0-9a-zA-Z,_-]+$/;
   if (un_l == 0) {
+    error1.innerHTML = "This is a required field";
     error.push('0');
-    error1.innerHTML = "This is a required field" + error;
+    u = u +1;
+  } else if (un_l < 3 || un_l > 29) {
+    error1.innerHTML = "Min length of 3 characters, max length of 29 characters";
+    error.push('0');
+    u = u +1;
+  } else if (!format.test(un)) {
+    error1.innerHTML = "Can only contain letters, numbers and underscore _ or dash -.";
+    error.push('0');
     u = u +1;
   } else {
     error1.innerHTML = "";
