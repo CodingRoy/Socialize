b11f7488015f6f8d7ed047008bb8ebe8fdeb34ec
CodingRoy <debessjes@hotmail.com>
2016-11-02 00:08:19 +0100

    Road to Socialize 1.0.0 Start with the css, resetting and normalising defaults.

--- view/header.php
+++ view/header.php
@@ -2,9 +2,22 @@
 <html>
 <head>
     <title>Socialize</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <script src="<?php echo URL ?>public/js/validate.js"></script>
     <script src="<?php echo URL ?>public/js/main.js"></script>
+    <link rel="stylesheet" type="text/css" href="public/css/main.css">
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
+  <header class="">
+    <a class="" href="<?php echo URL ?>"><i class="home icon"></i>Socialize</a>
+    <nav>
+      <ul>
+        <li><a class="" href="<?php echo URL ?>about"><i class="info icon"></i>About</a></li>
+        <li><a class="" href="<?php echo URL ?>dashboard"><i class="comments icon"></i>Dashboard</a></li>
+        <li><a class="" href="<?php echo URL ?>login"><i class="sign in icon"></i>Login</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main role="main" class="">
--- view/index/index.php
+++ view/index/index.php
@@ -1,15 +1,15 @@
-<p class=""> Welcome to Socialize! </p>
-<p class=""> Join us here! </p>
+<h1 class=""> Welcome to Socialize! </h1>
+<h2> Join us here! </h2>
 <form action="<?php echo URL ?>user/create" method="post" name="Register">
 	<p class="">Username</p>
 	<input class="" type="text" name="username" placeholder="Username" onblur="un_val()">
-	<p id="error1"></p>
+	<p class="" id="error1"></p>
 	<p class="">Email address</p>
-	<input class="" type="text" name="email" placeholder="Email address" onblur="mail_val()">
-	<p id="error2"></p>
-	<p class="">Create password</p>
-	<input class="" type="password" name="password" placeholder="Create password" onblur="pass_val()">
-	<p id="error3"></p>
+	<input class="" type="email" name="email" placeholder="Email address" onblur="mail_val()">
+	<p class="" id="error2"></p>
+	<p class="">Password</p>
+	<input class="" type="password" name="password" placeholder="Password" onblur="pass_val()">
+	<p class="" id="error3"></p>
 	<div class="g-recaptcha" data-sitekey="<?php echo SITEKEY ?>"></div>
 	<input class="" onclick="val()" type="submit" value="Register">
 </form>
