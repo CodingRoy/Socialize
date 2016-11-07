3e91ab54b68afa5000a59eca6b0cae637d0e0506
CodingRoy <debessjes@hotmail.com>
2016-11-07 18:05:35 +0100

    Road to Socialize 1.0.0 Update for form input and textarea and more

--- view/index/index.php
+++ view/index/index.php
@@ -1,16 +1,16 @@
-<h1 class=""> Welcome to Socialize! </h1>
+<h1> Welcome to Socialize! </h1>
 <h2> Join us here! </h2>
 <form action="<?php echo URL ?>user/create" method="post" name="Register">
 	<p class="ui field title">Username</p>
-	<input class="ui field rounded" type="text" name="username" placeholder="Username" onblur="un_val()">
+	<input class="ui field rounded" type="text" id="username" name="username" placeholder="Username" onblur="un_val()">
 	<p class="ui field error" id="error1"></p>
 	<p class="ui field title">Email address</p>
-	<input class="ui field rounded" type="email" name="email" placeholder="Email address" onblur="mail_val()">
+	<input class="ui field rounded" id="email" type="email" name="email" placeholder="Email address" onblur="mail_val()">
 	<p class="ui field error" id="error2"></p>
 	<p class="ui field title">Password</p>
-	<input class="ui field rounded" type="password" name="password" placeholder="Password" onblur="pass_val()">
+	<input class="ui field rounded" id="password" type="password" name="password" placeholder="Password" onblur="pass_val()">
 	<p class="ui field error" id="error3"></p>
 	<div class="g-recaptcha" data-sitekey="<?php echo SITEKEY ?>"></div>
-	<input class="ui button" onclick="val()" type="submit" value="Register">
+	<input class="ui primary button" onclick="val(1)" type="submit" value="Register">
 </form>
 <script src='https://www.google.com/recaptcha/api.js'></script>
--- view/login/index.php
+++ view/login/index.php
@@ -1,8 +1,10 @@
-<p class="title">Login</p>
-<form name="login_form" action="login/run" method="post">
-	<p class="">Username</p>
-	<input class="" type="text" name="username" placeholder="Username">
-	<p class="">Password</p>
-	<input class="" type="password" name="password" placeholder="Password">
-	<input type="submit" class="" value="Login">
+<h2>Login</h2>
+<form name="login_form" action="<?php echo URL ?>login/run" method="post">
+	<p class="ui field title">Username</p>
+	<input class="ui field rounded" type="text" name="username" id="username" placeholder="Username" onblur="un_val()">
+	<p class="ui field error" id="error1"></p>
+	<p class="ui field title">Password</p>
+	<input class="ui field rounded" type="password" name="password" id="password" placeholder="Password" onblur="pass_val()">
+	<p class="ui field error" id="error3"></p>
+	<input type="submit" class="ui primary button" value="Login" onclick="val()">
 </form>
